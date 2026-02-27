abbbfb6ab7345dbe6f06cf32c4389d8f34d848ef
Dario Quental <dhdq@hotmail.com>
2026-02-27 22:26:30 +0000

    add variaveis.py

--- Aula1/Aula.py
+++
@@ -1,2 +0,0 @@
-
-print("Hello World TPSI0226")
---
+++ Aula1/Variaveis.py
@@ -0,0 +1,42 @@
+'''
+comentar varias
+linhas
+'''
+#comentario na mesma linha
+
+# output para consola
+print("Hello World TPSI0226")
+
+#INT
+tel=9 # decimal e é 9
+print(type(tel))
+print(tel)
+#Variaveis dinamicas alteram de tipo automaticamente
+tel="9" # decimal vale 57
+print(type(tel))
+print(tel)
+
+#float
+med=2.3
+print(type(med))
+print(med)
+
+#string
+nom="Dario"
+print(type(nom))
+print(nom)
+
+#boolean
+flag=True
+print(type(flag))
+print(flag)
+
+# listas []   
+lista=[1,"dario",4.4,False,9]
+print(type(lista))
+print(lista)
+
+# tuples () parecidas com listas mas nao alteravel
+tupla=(1,3,4,6)
+print(type(tupla))
+print(tupla)
